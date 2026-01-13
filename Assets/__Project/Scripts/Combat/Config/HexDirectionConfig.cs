b2650c0afa5415d372869f281682beb32a8ef43d
Korden Ballass <korden.ballass@gmail.com>
2026-01-13 09:34:08 +0100

    Fixed movement diroections

--- Assets/__Project/Scripts/Combat/Config/HexDirectionConfig.cs
+++ Assets/__Project/Scripts/Combat/Config/HexDirectionConfig.cs
@@ -18,13 +18,13 @@ namespace Combat.Config
         public HexDirectionOffset[] directionOffsets = new HexDirectionOffset[6]
         {
             // Array index maps directly to sector index from DirectionToHexConverter
-            // Order empirically determined based on actual world coordinate behavior
-            new HexDirectionOffset(HexDirection.NW, new Vector2Int( 0, -1)), // Index 0 - Northwest (sector 0)
-            new HexDirectionOffset(HexDirection.NE, new Vector2Int(+1, -1)), // Index 1 - Northeast (sector 1)
-            new HexDirectionOffset(HexDirection.E,  new Vector2Int(+1,  0)), // Index 2 - East (sector 2)
-            new HexDirectionOffset(HexDirection.SE, new Vector2Int( 0, +1)), // Index 3 - Southeast (sector 3)
-            new HexDirectionOffset(HexDirection.SW, new Vector2Int(-1, +1)), // Index 4 - Southwest (sector 4)
-            new HexDirectionOffset(HexDirection.W,  new Vector2Int(-1,  0)), // Index 5 - West (sector 5)
+            // Order empirically determined: rotated by -2 sectors to correct 120° clockwise shift
+            new HexDirectionOffset(HexDirection.E,  new Vector2Int(+1,  0)), // Index 0 - East (sector 0)
+            new HexDirectionOffset(HexDirection.SE, new Vector2Int( 0, +1)), // Index 1 - Southeast (sector 1)
+            new HexDirectionOffset(HexDirection.SW, new Vector2Int(-1, +1)), // Index 2 - Southwest (sector 2)
+            new HexDirectionOffset(HexDirection.W,  new Vector2Int(-1,  0)), // Index 3 - West (sector 3)
+            new HexDirectionOffset(HexDirection.NW, new Vector2Int( 0, -1)), // Index 4 - Northwest (sector 4)
+            new HexDirectionOffset(HexDirection.NE, new Vector2Int(+1, -1)), // Index 5 - Northeast (sector 5)
         };
         
         [Header("World Direction Thresholds")]
